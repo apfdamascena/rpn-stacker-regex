7c2be562b249623b9076d3fdb75f547d1d7b6a36
Alex Damascena <apfd@cin.ufpe.br>
2023-04-20 22:55:44 -0300

    feature(rpn-stacker-regex): start rpn stacker regex with scanning project

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FileReader reader = new FileReader("/src/Calc1.stk");
+        List<Token> tokens = reader.read();
+        RPNStacker rpn = new RPNStacker();
+        Token output = rpn.calculate(tokens);
+
+        System.out.println("Saida: " + output.lexeme);
+
+    }
+}
---
+++ src/RPNStacker.java
@@ -0,0 +1,36 @@
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+public class RPNStacker {
+
+    private final Calculator calculator = new Calculator();
+
+    public Token calculate(List<Token> input){
+        Iterator<Token> iterator = input.iterator();
+        Stack<Token> stack = new Stack<>();
+
+        Token output;
+
+        while(iterator.hasNext()){
+            Token token = iterator.next();
+
+            if(stack.size() < 2) {
+                output = token;
+            } else {
+                Token left = stack.pop();
+                Token right = stack.pop();
+
+                Integer rightNumber = Integer.parseInt(right.lexeme);
+                Integer leftNumber = Integer.parseInt(left.lexeme);
+
+                Integer afterOperation = calculator.calculate(token.lexeme, rightNumber, leftNumber);
+                output = new Token(TokenType.NUM, afterOperation.toString());
+            }
+
+            stack.add(output);
+        }
+
+        return stack.pop();
+    }
+}
